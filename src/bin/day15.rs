9858b42c5f91abcf88cbfc777992d9d59bef38aa
Nick Peterson <nrpeterson@gmail.com>
2024-12-16 12:01:49 -0800

    Committing day 15 part 1 before I completely destroy it to also cover part 2

---
+++ src/bin/day15.rs
@@ -0,0 +1,165 @@
+use std::ops::{Index, IndexMut};
+use itertools::Itertools;
+use adventofcode2024::build_main;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+enum Contents { Empty, Box, Wall, Robot }
+
+#[derive(Debug, Copy, Clone)]
+enum Direction { Up, Down, Left, Right }
+use Direction::*;
+use crate::Contents::{Empty, Robot, Box, Wall};
+
+#[derive(Debug)]
+struct Level {
+    rows: usize,
+    cols: usize,
+    board: Vec<Vec<Contents>>,
+    robot_pos: (usize, usize)
+}
+
+impl Level {
+    fn get(&self, pos: (usize, usize)) -> Option<&Contents> {
+        let (i, j) = pos;
+        self.board.get(i).and_then(|row| row.get(j))
+    }
+
+    fn get_mut(&mut self, pos: (usize, usize)) -> Option<&mut Contents> {
+        let (i, j) = pos;
+        self.board.get_mut(i).and_then(|row| row.get_mut(j))
+    }
+
+    fn next_pos(&self, pos: (usize, usize), direction: Direction) -> Option<(usize, usize)> {
+        match (direction, pos) {
+            (Up, (i, _)) if i == 0 => None,
+            (Up, (i, j)) => Some((i - 1, j)),
+            (Down, (i, _)) if i == self.rows - 1 => None,
+            (Down, (i, j)) => Some((i + 1, j)),
+            (Left, (_, j)) if j == 0 => None,
+            (Left, (i, j)) => Some((i, j - 1)),
+            (Right, (_, j)) if j == self.cols - 1 => None,
+            (Right, (i, j)) => Some((i, j + 1))
+        }
+    }
+}
+
+impl Index<(usize, usize)> for Level {
+    type Output = Contents;
+
+    fn index(&self, index: (usize, usize)) -> &Self::Output {
+        self.get(index).unwrap()
+    }
+}
+
+impl IndexMut<(usize, usize)> for Level {
+    fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
+        self.get_mut(index).unwrap()
+    }
+}
+
+impl Level {
+    fn apply_move(&mut self, direction: Direction) -> Option<(usize, usize)> {
+        let cur_space = self.robot_pos;
+        let next_space = self.next_pos(self.robot_pos, direction)?;
+
+        if self[next_space] == Empty {
+            self[cur_space] = Empty;
+            self[next_space] = Robot;
+            self.robot_pos = next_space;
+            return Some(next_space)
+        }
+
+        let mut last_space_opt = Some(next_space);
+        while let Some(last_space) = last_space_opt {
+            if self[last_space] != Box {
+                break
+            }
+            last_space_opt = self.next_pos(last_space, direction);
+        }
+        let last_space = last_space_opt?;
+
+        if self[last_space] == Wall {
+            None
+        }
+        else {
+            self[cur_space] = Empty;
+            self[next_space] = Robot;
+            self[last_space] = Box;
+            self.robot_pos = next_space;
+            Some(next_space)
+        }
+    }
+}
+
+mod parse {
+    use nom::branch::alt;
+    use nom::character::complete::{char, multispace0, newline};
+    use nom::combinator::{map, opt, value};
+    use nom::IResult;
+    use nom::multi::{many1, separated_list1};
+    use nom::sequence::{preceded, separated_pair};
+    use crate::{Contents, Direction, Level};
+
+    pub fn parse_input(input: &str) -> IResult<&str, (Level, Vec<Direction>)> {
+        let cellp = alt((
+            value(Contents::Box, char('O')),
+            value(Contents::Empty, char('.')),
+            value(Contents::Wall, char('#')),
+            value(Contents::Robot, char('@'))
+        ));
+
+        let boardp = separated_list1(
+            newline,
+            many1(cellp)
+        );
+
+        let directionp = alt((
+            value(Direction::Up, char('^')),
+            value(Direction::Down, char('v')),
+            value(Direction::Left, char('<')),
+            value(Direction::Right, char('>'))
+        ));
+
+        let directionsp = many1(preceded(opt(newline), directionp));
+
+        let mut parser = map(
+            separated_pair(boardp, multispace0, directionsp),
+            |(board, directions)| {
+                let rows = board.len();
+                let cols = board[0].len();
+
+                let robot_pos = board.iter().enumerate()
+                    .filter_map(|(i, row)| {
+                        row.iter().enumerate()
+                            .find(|&(_, &contents)| contents == Contents::Robot)
+                            .map(|(j, _)| (i, j))
+                    })
+                    .next()
+                    .unwrap();
+
+                (Level { rows, cols, board, robot_pos }, directions)
+            }
+        );
+
+        parser(input)
+    }
+}
+
+fn part1(input: &str) -> usize {
+    let (mut level, directions) = parse::parse_input(input).unwrap().1;
+
+    for direction in directions {
+        level.apply_move(direction);
+    }
+
+    let mut total = 0;
+    for (i, j) in (0..level.rows).cartesian_product(0..level.cols) {
+        if level[(i, j)] == Box {
+            total += 100*i + j;
+        }
+    }
+
+    total
+}
+
+build_main!("day15.txt", "Part 1" => part1);
